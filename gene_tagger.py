974f682302b11d9e9bf01bb48a95c151a2b5437e
Javi Corvi <javicorvi@gmail.com>
2018-09-26 12:49:09 +0200

    dev draft

--- gene_tagger.py
+++ gene_tagger.py
@@ -73,7 +73,7 @@ def process(input_file, output_dir, index_id, index_text_to_tag):
             for line in file:
                 try:
                     data = re.split(r'\t+', line) 
-                    if(len(data)==5):
+                    if(len(data)>=6):
                         id = data[index_id]
                         text_to_tag = data[index_text_to_tag]
                         new_file.write(id+"|t|"+text_to_tag)
